261f59c5ef4967fbd65c0d21cd710bb3c3654afb
Yoshino-Yukitaro <sushiki.az@gmail.com>
2022-12-18 15:31:39 +0900

    feat: ヘッダーの大枠を作った

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        reggae: ["Reggae One", "cursive"],
       },
     },
   },
